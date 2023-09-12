5c88942c4bd85f8b4e00c3ebcaf848ce558aa4d1
timnilsson04 <timnils04@gmail.com>
2023-09-12 10:45:51 +0200

    ✨

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css'
 import {textChoices} from './textChoices.js'
 
+var Choice = localStorage.getItem('Choice');
+
+
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
@@ -13,7 +16,6 @@ function numberRandom() {
     }
     return number
 }
-
 var roulette
 function russianRoulette() {
     if (Math.random() < 0.166666) {
@@ -32,7 +34,6 @@ function svårRussianRoulette() {
     }
     return number
 }
-
 var svårasteRoulette
 function svårasteRussianRoulette() {
     if (Math.random() < 0.8333333) {
@@ -44,10 +45,9 @@ function svårasteRussianRoulette() {
 }
 
 let state = {}
-
 function startCV() {
   state = {}
-  showTextChoice(1)
+  showTextChoice(parseInt(Choice))
 }
 
 function showTextChoice(textChoiceIndex) {
@@ -68,30 +68,46 @@ function showTextChoice(textChoiceIndex) {
       if(textChoice.id == 14 || textChoice.id == 16){button.style.backgroundImage="url('/banana.gif')"
     button.style.height = "400px"
     button.style.width = "350px"
-    document.body.style.backgroundColor = "red"}
+    document.body.style.backgroundColor = "red"
+    Choice = 1}
     if(textChoice.id == 8 ){button.style.backgroundImage="url('/voices.gif')"
     button.style.height = "550px"
     button.style.width = "600px"
-    document.body.style.backgroundColor = "black"}
+    document.body.style.backgroundColor = "black"
+    Choice = 1}
     if(textChoice.id == 13 ){button.style.backgroundImage="url('/happyCat.gif')"
     button.style.height = "550px"
     button.style.width = "600px"
-    document.body.style.backgroundColor = "green"}
+    document.body.style.backgroundColor = "green"
+    Choice = 1}
     if(textChoice.id == 12 ){button.style.backgroundImage="url('/laserCat.gif')"
     button.style.height = "370px"
     button.style.width = "200px"
-    document.body.style.backgroundColor = "crimson"}
+    document.body.style.backgroundColor = "crimson"
+    Choice = 1}
     if(textChoice.id == 11 ){button.style.backgroundImage="url('/bellyCat.gif')"
     button.style.height = "550px"
     button.style.width = "600px"
-    document.body.style.backgroundColor = "blue"}
+    document.body.style.backgroundColor = "blue"
+    Choice = 1}
       button.innerText = option.text
       button.classList.add('btn')
       button.addEventListener('click', () => selectOption(option))
       optionButtonsElement.appendChild(button)
+
+
+      window.addEventListener("beforeunload", function (e) {
+        localStorage['Choice'] = textChoice.id
+        window.addEventListener("load", function (e) {
+           Choice = localStorage.getItem('Choice');
+
+       });
+     });
     }
   })
 }
+
+
 
 function showOption(option) {
   return option.requiredState == null || option.requiredState(state)
